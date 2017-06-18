430a9267024152f0427a23ff6fb245ea328e7790
roberthluo <robert.otherone@gmail.com>
2017-06-18 01:40:26 -0400

    Added a listening stream for the twitter bot

---
+++ twitter_bot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import json
+import oauth2 as oauth
+import tweepy
+
+from get_tweets import get_tweets
+
+#https://dev.twitter.com/overview/api/response-codes
+
+class twitter_bot(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
+
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_data disconnects the StreamListener
+            return False
